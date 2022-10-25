f409ea95516fc64812b1b87a302f53af3407f1e4
Max <manishsalunkhe58@gmail.com>
2022-10-25 19:21:07 +0530

    calculate Simple interest

---
+++ src/Java100Ques/InterestDemo.java
@@ -0,0 +1,11 @@
+package Java100Ques;
+
+import java.util.Scanner;
+
+public class InterestDemo {
+    public static void main(String[] args) {
+        float Principle = 13000,Rate=12,Time=2;
+        float SimpleInterest= (Principle*Rate*Time)/100;
+        System.out.println("Simple Interest = "+SimpleInterest);
+    }
+}
