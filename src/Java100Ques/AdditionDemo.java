4d084acb8f7afaa15894933437c41f1545bd8249
Max <manishsalunkhe58@gmail.com>
2022-10-25 19:11:52 +0530

    Adding two no.

---
+++ src/Java100Ques/AdditionDemo.java
@@ -0,0 +1,9 @@
+package Java100Ques;
+
+public class AdditionDemo {
+    public static void main(String args[]){
+        int a=5,b=5,sum;
+        sum=a+b;
+        System.out.print("Sum of two no. : "+sum);
+    }
+}
