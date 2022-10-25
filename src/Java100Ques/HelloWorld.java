4002bc4a695ac31721b7107e537c964aad2bf171
Max <manishsalunkhe58@gmail.com>
2022-10-25 19:02:17 +0530

    Hello world

---
+++ src/Java100Ques/HelloWorld.java
@@ -0,0 +1,9 @@
+package Java100Ques;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello!!!");
+        System.out.println("Manish <3");
+
+    }
+}
