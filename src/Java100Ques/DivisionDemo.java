39b7f2b8ecc3461c513960f5483f1711bf2c5945
Max <manishsalunkhe58@gmail.com>
2022-10-28 21:05:28 +0530

    Simple division of two no.

---
+++ src/Java100Ques/DivisionDemo.java
@@ -0,0 +1,9 @@
+package Java100Ques;
+
+public class DivisionDemo {
+    public static void main(String[] args) {
+        int a = 25, b = 5 ,c;
+        c=a/b;
+        System.out.println("Division : "+c);
+    }
+}
