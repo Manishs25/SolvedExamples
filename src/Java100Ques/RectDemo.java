0631ec89a7e994dc2ef8a3e0323e0b1b85cf2c74
Max <manishsalunkhe58@gmail.com>
2022-10-28 21:04:23 +0530

    Calculate Area and Perimeter of a rectangle

---
+++ src/Java100Ques/RectDemo.java
@@ -0,0 +1,12 @@
+package Java100Ques;
+
+public class RectDemo {
+    public static void main(String[] args) {
+        float length = 5, width = 7, area, perimeter;
+
+        area = length * width;
+        perimeter = 2 * (length + width);
+        System.out.println("Area of Rectangle :" + area);
+        System.out.println("Perimeter of Rectangle :" + perimeter);
+    }
+}
