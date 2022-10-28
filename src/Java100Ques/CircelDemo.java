ce5983981a67b6ab0ad2b0edfae5251fb8853aa5
Max <manishsalunkhe58@gmail.com>
2022-10-28 21:04:00 +0530

    Calculate Area and Perimeter of a circle

---
+++ src/Java100Ques/CircelDemo.java
@@ -0,0 +1,11 @@
+package Java100Ques;
+
+public class CircelDemo {
+    public static void main(String[] args) {
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        double perimeter = 2 * radius * Math.PI;
+        System.out.println("Area of a Circle : " + area);
+        System.out.println("Perimeter of a Cicle : " + perimeter);
+    }
+}
