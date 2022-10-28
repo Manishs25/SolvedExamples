1c28365c7dbc896bf620446ab665fb633c50d792
Max <manishsalunkhe58@gmail.com>
2022-10-28 21:03:09 +0530

    Arithmetic operations

---
+++ src/Java100Ques/ArithmeticOp.java
@@ -0,0 +1,17 @@
+package Java100Ques;
+
+public class ArithmeticOp {
+    public static void main(String[] args) {
+        int a = 25, b = 3;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        float div = a / b;
+        double rem= a % b;
+        System.out.println("Addition : " + sum);
+        System.out.println("Subsraction : " + sub);
+        System.out.println("Multiplication : " + mul);
+        System.out.println("Division : " + div);
+        System.out.println("Remainder : " + rem);
+    }
+}
