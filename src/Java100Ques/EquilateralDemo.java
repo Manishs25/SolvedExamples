035d5f00c04d53884a64cd9524267cccab484293
Max <manishsalunkhe58@gmail.com>
2022-10-29 20:25:59 +0530

    Area of equilateral triangle

---
+++ src/Java100Ques/EquilateralDemo.java
@@ -0,0 +1,9 @@
+package Java100Ques;
+
+public class EquilateralDemo {
+    public static void main(String[] args) {
+        float a = 4f;
+        double area = (1.73 * a * a) / 4;
+        System.out.println("Area of Equilateral Triangle is : " + area);
+    }
+}
